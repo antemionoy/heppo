a87f8e4c362a819c21da2543bd9745a89df9eeab
antemionoy <antemionoy@gmail.com>
2018-07-12 17:10:10 +0300

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,19 @@
+//= ../../bower_components/jquery/dist/jquery.min.js
+//= ../../bower_components/owl.carousel/dist/owl.carousel.min.js
+
+
+"use strict";
+
+
+
+$(function() {
+
+    $('.main-carousel').owlCarousel({
+        nav: true,
+        margin: 10,
+        items: 3,
+        dots: true
+
+    });
+
+});
