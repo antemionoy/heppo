06df45f2c647f83fe5317bb8d948c4128ce4916a
antemionoy <antemionoy@gmail.com>
2018-07-13 18:48:07 +0300

    three commit

--- src/js/main.js
+++ src/js/main.js
@@ -6,7 +6,32 @@
 
 
 
+function init_map() {
+    var myOptions = {
+        zoom: 13,
+        scrollwheel: false,
+        // draggable: false,
+        center: new google.maps.LatLng(60.033435, 30.244630),
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    map = new google.maps.Map(document.getElementById('gmap_canvas'), myOptions);
+
+    iconSrc = window.location.origin = '/img/marker.png';
+
+    ms0 = new google.maps.Marker({
+        position: new google.maps.LatLng(60.033435, 30.244630),
+        map: map,
+        icon: '/img/marker.png'
+    });
+}
+
+
+
 $(function() {
+
+    if (typeof(google) != 'undefined') {
+        google.maps.event.addDomListener(window, 'load', init_map);
+    }
 
     $('.main-carousel').owlCarousel({
         nav: true,
